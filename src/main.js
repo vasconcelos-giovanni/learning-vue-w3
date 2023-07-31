dac425a5adae95e2d5f31a2219b378662013cf6f
Giovanni Vasconcelos <giovannivmedeiros@gmail.com>
2023-07-31 09:58:39 -0300

    update router to have the repository name as base URL

--- src/main.js
+++ src/main.js
@@ -2,26 +2,25 @@
 /*                    main.js tells how to run the Vue code                   */
 /* -------------------------------------------------------------------------- */
 
-import { createApp } from 'vue';
-import App from './App.vue';
-import { createRouter, createWebHistory } from 'vue-router';
+import { createApp } from "vue";
+import App from "./App.vue";
+import { createRouter, createWebHistory } from "vue-router";
 
-import FoodItem from './components/FoodItem.vue';
-import TodoItem from './components/TodoItem.vue';
+import FoodItem from "./components/FoodItem.vue";
+import TodoItem from "./components/TodoItem.vue";
 import FoodSlot from "./components/FoodSlot.vue";
 import Routed from "./components/Routed.vue";
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes: [
-        { path: '/routed', component: Routed }
-    ]
+  history: createWebHistory(),
+  base: "/learning-vue-w3/",
+  routes: [{ path: "/routed", component: Routed }],
 });
 
 const app = createApp(App);
 app.use(router);
-app.component('food-item', FoodItem);
-app.component('todo-item', TodoItem);
-app.component('food-slot', FoodSlot);
+app.component("food-item", FoodItem);
+app.component("todo-item", TodoItem);
+app.component("food-slot", FoodSlot);
 
-app.mount('#app');
+app.mount("#app");
